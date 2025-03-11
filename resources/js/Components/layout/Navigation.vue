611bd2945660385369bbed838d662fd7d8a101b3
urosdimi1011 <dimitrijevicuros1011@gmail.com>
2025-03-11 20:50:26 +0100

    Phosphor icons

--- resources/js/Components/layout/Navigation.vue
+++ resources/js/Components/layout/Navigation.vue
@@ -2,6 +2,8 @@
 import Stepper from 'primevue/stepper';
 import StepList from 'primevue/steplist';
 import StepPanels from 'primevue/steppanels';
+import {PhUpload,PhEye,PhTable} from "@phosphor-icons/vue";
+
 import StepItem from 'primevue/stepitem';
 import Step from 'primevue/step';
 import StepPanel from 'primevue/steppanel';
@@ -11,9 +13,9 @@ import StepPanel from 'primevue/steppanel';
     <div class="card flex justify-center">
         <Stepper value="1" class="basis-[50rem]">
             <StepList>
-                <Step value="1">Header I</Step>
-                <Step value="2">Header II</Step>
-                <Step value="3">Header III</Step>
+                <Step value="1"><PhUpload :size="32" /></Step>
+                <Step value="2"><PhEye :size="32" /></Step>
+                <Step value="3"><PhTable :size="32" /></Step>
             </StepList>
             <StepPanels>
                 <StepPanel v-slot="{ activateCallback }" value="1">
